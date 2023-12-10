c25c93eb08de9564f2dc7641664c088d85fd33d3
AYUB SHAH <ayubshah810@gmail.com>
2023-12-11 01:08:15 +0500

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, toggleTodo, deleteTodo } from '../actions/Action';
+import '../styles/style.css';
+
+const TodoList = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  let inputRef = React.createRef();
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value.trim();
+    if (text !== '') {
+      dispatch(addTodo(text));
+      inputRef.current.value = '';
+    }
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <div className="todo-list-container">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input type="text" ref={inputRef} placeholder="Add new todo" />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              className={todo.completed ? 'completed' : ''}
+              onClick={() => handleToggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
